ac787d8c63f43f9a05e8ff68c8cc0218d2ab9097
Paul Ng <paul.ng@rms.nsw.gov.au>
2015-04-22 13:49:37 +1000

    Retreive tollgate data

--- routes/index.js
+++ routes/index.js
@@ -18,37 +18,6 @@ app.options("*", function (req, res) {
     res.send(200)
 });
 
-// error handlers
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
 
 
 /* GET home page. */
